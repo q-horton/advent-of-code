76beddbd1849d2f282d3c6e2871c49dabf42f173
Quinn Horton <106427075+q-horton@users.noreply.github.com>
2025-12-01 13:52:31 +1000

    Added 2024 Day 2

--- 2024/Day 2/code.py
+++ 2024/Day 2/code.py
@@ -1,13 +1,58 @@
-
+def parse(lines):
+    result = []
+    for line in lines:
+        var = [int(i) for i in line.split()]
+        result.append(var)
+    return result
 
 def run(inp):
-    print("Hello World!")
+    reports = parse(inp)
+    num_safe = 0
+    for rep in reports:
+        diff = rep[0] - rep[1]
+        if diff == 0:
+            continue
+        dir = diff / abs(diff)
+        safe = True
+        for i in range(len(rep) - 1):
+            diff = rep[i] - rep[i + 1]
+            if not (1 <= abs(diff) <= 3):
+                safe = False
+                break
+            if diff / abs(diff) != dir:
+                safe = False
+                break
+        if safe:
+            num_safe += 1
+    print(num_safe)
+
+def check_safe(rep):
+    diff = rep[0] - rep[1]
+    if diff == 0:
+        return False
+    dir = diff / abs(diff)
+    for i in range(len(rep) - 1):
+        diff = rep[i] - rep[i + 1]
+        if not (1 <= abs(diff) <= 3):
+            return False
+        if diff / abs(diff) != dir:
+            return False
+    return True
 
 def run2(inp):
-    print("Hello World!")
+    reports = parse(inp)
+    num_safe = 0
+    for rep in reports:
+        if check_safe(rep):
+            num_safe += 1
+        else:
+            for i in range(len(rep)):
+                if check_safe(rep[:i] + rep[i + 1:]):
+                    num_safe += 1
+                    break
+    print(num_safe)
 
 with open('./input.txt') as f:
     lines = f.readlines()
 
-run(lines)
-# run2(lines)
+run2(lines)
