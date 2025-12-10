4a8fabc85e9b294bf53f6e44d3bf272f49c06b40
Quinn Horton <106427075+q-horton@users.noreply.github.com>
2025-12-11 01:52:21 +1000

    Improved solution to Day 9.

--- 2025/Day 9/code.py
+++ 2025/Day 9/code.py
@@ -14,9 +14,44 @@ def run(inp):
                 max_area = area
     print(max_area)
 
+def in_shape(corners, edges, point):
+    if point in edges:
+        return True
+    intersects = 0
+    on_edge = False
+    left = False
+    for x in range(-1, point[0]):
+        if on_edge:
+            if (x, point[1]) in corners:
+                on_edge = False
+                if (left and (x, point[1] + 1) in edges) or \
+                        not (left or (x, point[1] + 1) in edges):
+                    intersects += 1
+        elif (x, point[1]) in corners:
+            on_edge = True
+            if (x, point[1] + 1) in edges:
+                left = False
+            else:
+                left = True
+        elif (x, point[1]) in edges:
+            intersects += 1
+    return True if intersects % 2 == 1 else False
+
 def run2(inp):
     data = parse(inp)
     max_area = 0
+    edges = set()
+    for i in range(len(data)):
+        x_vals = [data[i][0], data[(i+1) % len(data)][0]]
+        x_vals.sort()
+        y_vals = [data[i][1], data[(i+1) % len(data)][1]]
+        y_vals.sort()
+        if x_vals[0] == x_vals[1]:
+            for y in range(y_vals[0], y_vals[1] + 1):
+                edges.add((x_vals[0], y))
+        else:
+            for x in range(x_vals[0], x_vals[1] + 1):
+                edges.add((x, y_vals[0]))
     for i in range(len(data)):
         for j in range(i + 1, len(data)):
             x_vals = [data[i][0], data[j][0]]
@@ -44,10 +79,10 @@ def run2(inp):
                                 break
                             if x >= x_vals[1] and x_vals[1] > nx:
                                 break
-                    # Shouldn't work yet as it doesn't account for shapes
-                    # outside the loop with no edges running through them
-                    # It did though...
                 else:
+                    midpoint = (sum(x_vals) // 2, sum(y_vals) // 2)
+                    if not in_shape(data, edges, midpoint):
+                        continue
                     print(f"New max area: {area}. Currently at {i * len(data) + j} of {len(data) ** 2}")
                     max_area = area
     print(max_area)
